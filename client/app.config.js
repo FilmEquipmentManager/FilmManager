0c890fc9ad1b572afc2b19a1bf397b291d27631f
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-04-25 16:49:54 +0800

    Kotlin version update

--- client/app.config.js
+++ client/app.config.js
@@ -1,81 +1,67 @@
 require("dotenv/config");
 
-module.exports = ({ config }) => ({
-    ...config,
+module.exports = () => ({
+  expo: {
     name: "Film Manager",
-    slug: "film-manager",
+    slug: "client",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
     userInterfaceStyle: "automatic",
     splash: {
-        image: "./assets/images/splash.png",
-        resizeMode: "contain",
-        backgroundColor: "#ffffff",
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
     assetBundlePatterns: ["**/*"],
-    expo: {
-        plugins: [
-          [
-            "expo-build-properties",
-            {
-              android: {
-                kotlinVersion: "1.9.25"
-              }
-            }
-          ]
-        ]
-    },
     ios: {
-        ...config.ios,
-        supportsTablet: true,
-        bundleIdentifier: "com.sadliquid.Film-Manager",
+      supportsTablet: true,
+      bundleIdentifier: "com.sadliquid.Film-Manager",
     },
     android: {
-        ...config.android,
-        package: "com.sadliquid.filmmanager",
-        adaptiveIcon: {
-            foregroundImage: "./assets/images/adaptive-icon.png",
-            backgroundColor: "#ffffff",
-        },
+      package: "com.sadliquid.filmmanager",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
     },
     web: {
-        ...config.web,
-        bundler: "metro",
-        output: "static",
-        favicon: "./assets/images/favicon.png",
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
     plugins: [
-        "expo-router",
-        "expo-font",
-        [
-            "expo-build-properties",
-            {
-                ios: {
-                    newArchEnabled: true,
-                },
-                android: {
-                    newArchEnabled: true,
-                },
-            },
-        ],
+      "expo-router",
+      "expo-font",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            newArchEnabled: true,
+          },
+          android: {
+            newArchEnabled: true,
+            kotlinVersion: "1.9.25",
+          },
+        },
+      ],
     ],
     experiments: {
-        typedRoutes: true,
+      typedRoutes: true,
     },
-    newArchEnabled: true,
-    extra: { // Add all .env variables here
-        BASE_URL: process.env.BASE_URL,
-        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-        FIREBASE_DB_URL: process.env.FIREBASE_DB_URL,
-        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-        FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
-        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-        eas: {
-            projectId: "d6f7b2a9-6744-42a0-9bea-494dfbeb8ccf"
-        }
+    extra: {
+      BASE_URL: process.env.BASE_URL,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_DB_URL: process.env.FIREBASE_DB_URL,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      eas: {
+        projectId: "d6f7b2a9-6744-42a0-9bea-494dfbeb8ccf",
+      },
     },
+  },
 });
